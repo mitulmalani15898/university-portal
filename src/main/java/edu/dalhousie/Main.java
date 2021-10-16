4c3f9d80f1aa9c0538d511c1270a4f6520fa4b90
Abhishek karthik Manikandan <ab620101@dal.ca>
2021-10-16 13:02:43 -0300

    CHECKING DATABASE CONNECTIVITY AND FIXING PIPELINE ISSUE

--- src/main/java/edu/dalhousie/Main.java
+++ src/main/java/edu/dalhousie/Main.java
@@ -1,7 +1,11 @@
 package edu.dalhousie;
 
+import edu.dalhousie.database.DatabaseConnectivity;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello world");
+        DatabaseConnectivity db = new DatabaseConnectivity();
+        db.connectWithDataBase();
     }
 }
--- src/main/java/edu/dalhousie/database/DatabaseConnectivity.java
+++ src/main/java/edu/dalhousie/database/DatabaseConnectivity.java
@@ -1,2 +1,35 @@
-package edu.dalhousie.database;public class DatabaseConnectivity {
+package edu.dalhousie.database;
+import java.sql.*;
+public class DatabaseConnectivity {
+    private Connection connection = null;
+    private Statement statement = null;
+    private ResultSet resultSet = null;
+    public void connectWithDataBase() throws Exception {
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            connection = DriverManager
+                    .getConnection("jdbc:mysql://db-5308.cs.dal.ca/CSCI5308_9_DEVINT?"
+                            + "user=CSCI5308_9_DEVINT_USER&password=zoDooSheupaefau1");
+
+            System.out.println("DATABASE CONNECTIVITY ESTABLISHED");
+
+            statement = connection.createStatement();
+            resultSet = statement
+                    .executeQuery("select * from test");
+            showResultSet(resultSet);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            connection.close();
+        }
+
+    }
+
+    private void showResultSet(ResultSet resultSet) throws SQLException {
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("name"));
+        }
+    }
 }
