c1ac294b98b8921b6db34cb59eacf118e910c44d
Vignesh Panemangalore Nayak <vg866663@dal.ca>
2021-10-24 12:15:42 -0300

    Created a view class and refactored WelcomeClass accordingly

--- src/main/java/edu/dalhousie/WelcomeClass.java
+++ src/main/java/edu/dalhousie/WelcomeClass.java
@@ -1,4 +1,6 @@
 package edu.dalhousie;
+import edu.dalhousie.presentation.StudentView;
+
 import java.io.Console;
 import java.util.Scanner;
 
@@ -11,39 +13,31 @@ public class WelcomeClass {
     private String kExit = "Login as a Faculty";
 
     public void displayWelcomeScreen() {
-//        Console console = System.console();
-//        Scanner sc = new Scanner(console.reader());
-//
-//        if (console != null) {
-//            console.writer().println("1 " + registerAsStudent + "\n" + "2 " + registerAsFaculty + "\n" + "3 " + loginAsStudent + "\n" + "4 " + loginAsFaculty + "\n" + "5 " + "Exit\n");
-//            console.writer().println("Enter your choice\n");
-//            choice = sc.nextInt();
-//        }
-
+        StudentView view = new StudentView();
         Scanner sc = new Scanner(System.in);
-        System.out.println("WELCOME TO UNIVERSITY PORTAL");
-        System.out.println("1 " + kRegisterAsStudent + "\n" + "2 " + kRegisterAsFaculty + "\n" + "3 " + kLoginAsStudent + "\n" + "4 " + kLoginAsFaculty + "\n" + "5 " + kExit);
-        System.out.println("Enter your choice\n");
-        choice = sc.nextInt();
+        view.showMessage("WELCOME TO UNIVERSITY PORTAL");
+        view.showMessage("1 \" + kRegisterAsStudent + \"\\n\" + \"2 \" + kRegisterAsFaculty + \"\\n\" + \"3 \" + kLoginAsStudent + \"\\n\" + \"4 \" + kLoginAsFaculty + \"\\n\" + \"5 \" + kExit \n");
+        view.showMessage("Enter your choice");
+        choice = view.getMessage();
 
         switch(choice) {
             case 1:
                 //call method
 //                StudentMainClass student = new StudentMainClass();
 //                student.displayStudentMenu();
-                System.out.println("You selected 1");break;
+                view.showMessage("selected 1"); break;
             case 2:
                 //call method
-                System.out.println("You selected 2");break;
+                view.showMessage("selected 2"); break;
             case 3:
                 //call method
-                System.out.println("You selected 3");break;
+                view.showMessage("selected 3"); break;
             case 4:
                 //call method
-                System.out.println("You selected 4");break;
+                view.showMessage("selected 4"); break;
             case 5:
                 //call method
-                System.out.println("You selected 5");break;
+                view.showMessage(" selected 5"); break;
             default: break;
         }
 
